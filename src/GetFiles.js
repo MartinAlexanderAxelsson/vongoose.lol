300a0e7c7ee717f0a11af3ef630c6bff96bdde7c
MartinAlexanderAxelsson <axelsson.mejl@gmail.com>
2021-10-17 02:11:58 +0200

    set up

--- src/GetFiles.js
+++ src/GetFiles.js
@@ -1,28 +1,28 @@
-import { useState, useEffect } from "react"
-import app from "./firebaseConfig"
-import {
-  getFirestore,
-  collection,
-  query,
-  where,
-  getDocs,
-} from "firebase/firestore"
+// import { useState, useEffect } from "react"
+// import app from "./firebaseConfig"
+// import {
+//   getFirestore,
+//   collection,
+//   query,
+//   where,
+//   getDocs,
+// } from "firebase/firestore"
 
-const db = getFirestore(app)
-const qry = query(collection(db, "assets"))
+// const db = getFirestore(app)
+// const qry = query(collection(db, "assets"))
 
-function GetFiles() {
-  const [data, setData] = useState([])
+// function GetFiles() {
+//   const [data, setData] = useState([])
 
-  useEffect(() => {
-    const getData = async () => {
-      const fetch = await getDocs(qry)
-      fetch.forEach((doc) => {
-        setData(doc.data())
-      })
-    }
+//   useEffect(() => {
+//     const getData = async () => {
+//       const fetch = await getDocs(qry)
+//       fetch.forEach((doc) => {
+//         setData(doc.data())
+//       })
+//     }
 
-    getData()
-  }, [])
-}
-export default GetFiles
+//     getData()
+//   }, [])
+// }
+// export default GetFiles
